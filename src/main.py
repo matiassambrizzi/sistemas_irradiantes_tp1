b12a60e62f7f5766d5c5519dedb373884a103fed
matiassambrizzi <msambrizzi@fi.uba.ar>
2020-05-03 20:09:53 -0300

    Agregue manejo de direcciones con Path de pathlib, si el directorio imagenes no existe entonces crea uno

--- src/main.py
+++ src/main.py
@@ -6,14 +6,18 @@ Srcipt para realizar el trabajo practico numero 1 de la materia Sistemas Irradia
 
 Numpy define el tipo de dato ndarray que es una coleccion de datos del mismo tipo
 """
+from pathlib import Path #TODO: ver
+from smithplot import SmithAxes
 import numpy as np
 import matplotlib.pyplot as plt
-from smithplot import SmithAxes
 
-PREV_DIR = "../"
-IMG_DIR = "imagenes/"
+
+
+WORKING_DIR = Path.cwd()
+
+IMG_DIR = "imagenes"
 SRC_DIR = "src/"
-IMG_TYPE = ".png"
+IMG_TYPE = "png"
 S1P_DATA_DIR = "ArchivosDeMedicion/ArchivosS1p/"
 S2P_DATA_DIR = "ArchivosDeMedicion/ArchivosS2p/"
 
@@ -36,7 +40,8 @@ def read_data(location, cols, delim):
 def array_to_complex(arr, real, imag):
     return [x[real] + x[imag]*1j for x in arr]
 
-"""@brief
+"""@brief transformar los coeficiente de reflexion en impedancia para poder \
+    plotear en la carta de smith
 """
 def reflexion_coefficient_to_impedance(arr, caracteristic_impedance):
     return [caracteristic_impedance * (1+x)/(1-x) for x in arr]
@@ -44,30 +49,35 @@ def reflexion_coefficient_to_impedance(arr, caracteristic_impedance):
 """@brief plot smith chart function
 """
 def plot_smith_chart(input_impedance, caracteristic_impedance, file_name):
+    directory = WORKING_DIR.parent / IMG_DIR
+    if directory.is_dir() == False:
+        directory.mkdir()
     fig = plt.figure(figsize=(10, 8))
     SmithAxes.update_scParams(axes_impedance=caracteristic_impedance)
     plt.subplot(1, 1, 1, projection='smith', grid_major_enable=True)
     plt.plot(input_impedance, datatype=SmithAxes.Z_PARAMETER)
     plt.show()
-    fig.savefig(PREV_DIR+IMG_DIR+file_name+IMG_TYPE, bbox_inches='tight', dpi=150)
+    fig.savefig(directory / f'{file_name}.{IMG_TYPE}',\
+                bbox_inches='tight', dpi=150)
 """@brief funcion principal
 """
 def main():
     caracteristic_impedance: int = 50
-
-    s1p_file = PREV_DIR + S1P_DATA_DIR + 'AutoSave'
-    #list comprehension
-    s1p_files = [s1p_file+str(n)+'.s1p' for n in range(1, 10)]
+    #TODO: tratar de manejar todo usando Path()
+    s1p_file_dir = WORKING_DIR.parent / S1P_DATA_DIR
+    #Uso un generador, esto me ahorra memoria porque no crea una lista
+    #Una vez que uso el gnerador ya no lo puedo volver a usar
+    s1p_files = (s1p_file_dir / f'AutoSave{n}.s1p' for n in range(1, 10));
     #Ploteo la carta de smith para todos los archivos
     for file in s1p_files:
-        print(file)
+        print('Ploteando', file.stem)
         data = read_data(file,
                          (0, 1, 2), '\t')
         s_11_parameter = array_to_complex(data, 1, 2)
         input_impedance = reflexion_coefficient_to_impedance(s_11_parameter,
                                                              caracteristic_impedance)
         plot_smith_chart(input_impedance, caracteristic_impedance,
-                         file.split('/')[-1])
+                         file.name)
 
 if __name__ == '__main__':
     main()
