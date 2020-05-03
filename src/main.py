a70bb86a067e9fb3cf1f31bc2c0789fb83db7c84
matiassambrizzi <msambrizzi@fi.uba.ar>
2020-05-03 12:40:50 -0300

    agregue funcion para plotear la carta de sith

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat May  2 18:39:25 2020
+"""Created on Sat May  2 18:39:25 2020
 @author: matias
 Srcipt para realizar el trabajo practico numero 1 de la materia Sistemas Irradiantes FIUBA
 
@@ -9,62 +8,71 @@ Numpy define el tipo de dato ndarray que es una coleccion de datos del mismo tip
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from smithplot import SmithAxes 
-from smithplot import smithhelper
+from smithplot import SmithAxes
 
-"""
-@brief funcion para leer data de un archivo
+PREV_DIR = "../"
+IMG_DIR = "imagenes/"
+SRC_DIR = "src/"
+
+"""@brief funcion para leer data de un archivo
 @param location es la ubicacion del archivo
 @param cols tupla que indica las columnas que se quieren leer
 @param delim es el delimitador
 @return s los datos leidos
 """
-def readData(location,cols,delim):
-    s = np.genfromtxt(location \
-                      ,delimiter=delim,skip_header=8,usecols=cols);
-    return s;
 
-"""
-@brief transformar un array de dos dimensiones a un numero complejo \
+def read_data(location, cols, delim):
+    data = np.genfromtxt(location \
+                      , delimiter=delim, skip_header=8, usecols=cols)
+    return data
+
+"""@brief transformar un array de dos dimensiones a un numero complejo \
     input = [Re,Im]
 @return lista de numeros complejos
 """
-def arrayToComplex(arr,real,imag):
-    return list(map(lambda x: x[real] + x[imag]*1j,arr));
+def array_to_complex(arr, real, imag):
+    return [x[real] + x[imag]*1j for x in arr]
 
+"""@brief
 """
-@brief
+def reflexion_coefficient_to_impedance(arr, caracteristic_impedance):
+    return [caracteristic_impedance * (1+x)/(1-x) for x in arr]
+
+"""@brief plot smith chart function
 """
-def reflexionToImpedance(arr,zo):
-    return list(map(lambda  x: zo * (1 + x) / (1 - x),arr));
-"""
-@brief funcion principal
+def plot_smith_chart(input_impedance, caracteristic_impedance,file_name):
+    fig = plt.figure(figsize=(10, 8))
+    SmithAxes.update_scParams(axes_impedance=caracteristic_impedance)
+    plt.subplot(1, 1, 1, projection='smith', grid_major_enable=True)
+    plt.plot(input_impedance, datatype=SmithAxes.Z_PARAMETER)
+    plt.show()
+    fig.savefig(PREV_DIR+IMG_DIR+file_name+'.png',bbox_inches='tight',dpi=150)
+    
+    
+"""@brief funcion principal
 """
 def main():
-    
-    zo = 50;
-    data = readData('../ArchivosDeMedicion/ArchivosS1p/AutoSave1.s1p',(0,1,2),'\t');
-    #Transform data to complex number    
-    s11 = arrayToComplex(data,1,2)
+
+    caracteristic_impedance: int = 50
+    data = read_data('../ArchivosDeMedicion/ArchivosS1p/AutoSave1.s1p', \
+                     (0, 1, 2), '\t')
+    #Transform data to complex number
+    s_11_parameter = array_to_complex(data, 1, 2)
     #Transfomr reflexion to impedance
-    z = reflexionToImpedance(s11,zo);
-    
-    plt.figure(figsize=(8,8));
-    SmithAxes.update_scParams(axes_impedance = 50);
-    plt.subplot(1,1,1, projection='smith',grid_major_enable=True);
-    plt.plot(z, datatype=SmithAxes.Z_PARAMETER)
-    #leg = plt.legend(loc="lower right", fontsize=12)
-    plt.show()
-    
+    input_impedance = reflexion_coefficient_to_impedance(s_11_parameter, \
+                                             caracteristic_impedance)
+
+    plot_smith_chart(input_impedance,caracteristic_impedance,'first')
+
 if __name__ == '__main__':
-    main();
-    
-    
+    main()
+
+
 """
 TODO: carta de smith
-plt.figure(figsize=(8,8)) 
+plt.figure(figsize=(8,8))
 SmithAxes.update_scParams(axes_impedance = 75)
-plt.subplot(1,1,1, projection='smith',grid_major_enable=True) 
+plt.subplot(1,1,1, projection='smith',grid_major_enable=True)
 plt.plot(zl+0j , label="50+50j", datatype=SmithAxes.Z_PARAMETER)
 leg = plt.legend(loc="lower right", fontsize=12)
 plt.show()
